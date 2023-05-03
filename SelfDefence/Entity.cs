10f806659b5f06456bec14ca92a6772435ee3762
UweSilver <40294263+syutotyou@users.noreply.github.com>
2023-05-03 20:53:30 +0900

    wip

---
+++ SelfDefence/Entity.cs
@@ -0,0 +1,30 @@
+﻿using Altseed2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfDefence
+{
+    interface Entity
+    {
+        public Vector2I Position { get; }
+        public TransformNode Node { get; }
+    }
+
+    internal class Player : Entity
+    {
+        public Vector2I Position { get; set; }
+
+        public int WalkableLandState = 20;
+
+        public TransformNode Node { get; set; }
+    }
+
+    class Lod : Entity
+    {
+        public Vector2I Position { get; set; }
+        public TransformNode Node { get; set; }
+    }
+}
--- SelfDefence/Game.cs
+++ SelfDefence/Game.cs
@@ -12,14 +12,18 @@ namespace SelfDefence
         Scene scene;
         TileField field;
 
-        TileFieldObjectLayer<int> Land = new(); // 0 is none, 100 is full
+        TileFieldObjectLayer<(int, RectangleNode)> LandStatus = new(); // 0 is none, 100 is full
+        TileFieldObjectLayer<CircleNode> Lod = new();
+        TileFieldObjectLayer<Entity> Entity = new();
 
         Player player = new();
+        Lod lod = new();
         
         public Game()
         {
             scene = new(0b1);
 
+            //init field
             var fieldSize = new Vector2I(55, 55);
             var fieldUnitSize = new Vector2F(30, 30);
             field = new(fieldSize, fieldUnitSize);
@@ -28,7 +32,7 @@ namespace SelfDefence
             {
                 for(int j = 0; j < fieldSize.Y; j++)
                 {
-                    int value = new Random().Next(0, 100);
+                    int value = new Random().Next(100, 100);
 
                     var t = new RectangleNode();
                     t.RectangleSize = fieldUnitSize * 0.9f;
@@ -37,11 +41,13 @@ namespace SelfDefence
                     t.Position = !worldPos.Item2 ? worldPos.Item1 : new Vector2F(0, 0);
                     t.Color = new Color((byte)(100 * value / 100), 80, 10);
 
-                    Land.LayerObjects.Add(new Vector2I(i, j), value);
+                    LandStatus.LayerObjects.Add(new Vector2I(i, j), (value, t));
 
                     scene.AddNode(t);
                 }
             }
+
+            //init player
             {
                 player.Position = new Vector2I(10, 10);
                 var playerObj = new CircleNode();
@@ -51,14 +57,31 @@ namespace SelfDefence
                 playerObj.Position = !worldPos.Item2 ? worldPos.Item1 : new Vector2F(0, 0);
                 playerObj.Color = new Color(10, 10, 150);
                 player.Node = playerObj;
+
+                Entity.LayerObjects.Add(player.Position, player);
                 scene.AddNode(playerObj);
             }
 
+            //init lod
+            {
+                var lodObj = new CircleNode();
+                lodObj.Radius = 20;
+                lodObj.VertNum = 5;
+                var worldPos = field.AddressToPosition(new Vector2I(20, 20));
+                lodObj.Position = !worldPos.Item2 ? worldPos.Item1 : new Vector2F(0, 0);
+                lodObj.Color = new Color(250, 250, 250);
+
+                lod.Node = lodObj;
+                lod.Position = new Vector2I(20, 20);
+                Entity.LayerObjects.Add(new Vector2I(20, 20), lod);
+                scene.AddNode(lodObj);
+            }
         }
 
         public void Update()
         {
             UpdatePlayer();
+            UpdateLand();
             UpdateGameSystem();
         }
 
@@ -66,11 +89,13 @@ namespace SelfDefence
         {
             void Move(Vector2I vec)
             {
-                if (Land.LayerObjects.TryGetValue(player.Position + vec, out int state))
+                if (LandStatus.LayerObjects.TryGetValue(player.Position + vec, out var state) && !Entity.LayerObjects.ContainsKey(player.Position + vec))
                 {
-                    if (state >= player.WalkableLandState)
+                    if (state.Item1 >= player.WalkableLandState)
                     {
+                        Entity.LayerObjects.Remove(player.Position);
                         player.Position += vec;
+                        Entity.LayerObjects.Add(player.Position, player);
                     }
                 }
 
@@ -97,6 +122,16 @@ namespace SelfDefence
         void UpdateGameSystem()
         {
 
+        }
+
+        void UpdateLand()
+        {
+            var lodPos = lod.Position;
+
+            var target = new Vector2I(new Random().Next(0, field.Size.X), new Random().Next(0, field.Size.Y));
+
+            LandStatus.LayerObjects[target] = (LandStatus.LayerObjects[target].Item1 - 50, LandStatus.LayerObjects[target].Item2);
+            LandStatus.LayerObjects[target].Item2.Color = new Color((byte)(100 * LandStatus.LayerObjects[target].Item1 / 100), 80, 10);
         }
     }
 }
